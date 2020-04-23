333240f97a4cb4a645d5eecc344c4dc80a4b4d77
Marie Malarme <malarmemarie@gmail.com>
2020-04-23 17:49:50 +0100

    Created Div and Span components + displayed all the created colors on the main page.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,48 @@
 import React from 'react'
+import { Div } from './lib.js'
+import { colors } from './style.js'
 import './App.css'
 
-const App = () => <div>Marie Malarme</div>
+const App = () => {
+  return (
+    <Div
+      style={{
+        display: 'flex',
+        flexWrap: 'wrap',
+        justifyContent: 'space-between',
+        width: '100%',
+        padding: '5rem',
+      }}
+    >
+      {colors.map(([key, value]) => (
+        <Div
+          style={{
+            display: 'flex',
+            flexDirection: 'column',
+            width: 'calc(100% / 9)',
+            alignItems: 'center',
+            marginTop: `3rem`,
+            marginBottom: `3rem`,
+            marginRight: `${(key === 'black' && 'calc((100% / 9 * 7))') ||
+              '0'}`,
+          }}
+        >
+          <Div
+            style={{
+              width: '30px',
+              height: '30px',
+              background: value,
+              borderRadius: '50%',
+              boxShadow: '0 0 25px rgba(0, 0, 0, 0.25)',
+            }}
+          />
+          <Div style={{ marginTop: '1rem', fontFamily: 'monospace' }}>
+            {key}
+          </Div>
+        </Div>
+      ))}
+    </Div>
+  )
+}
 
 export default App
